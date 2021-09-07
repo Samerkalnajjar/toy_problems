ee4a6a9854230bb4ab5fca1d19b1736002438384
Samerkalnajjar <samer.alnajjar@optimumpartners.co>
2021-09-07 14:33:51 +0300

    snake_assignment

--- toSnakeCase/index.test.js
+++ toSnakeCase/index.test.js
@@ -10,8 +10,44 @@
   [{ abcD: 1, user: { userId: 2 } }]  gets converted to [{ abc_d: 1, user: { user_id: 2 } }]
  */
 
-  function toSnakeCase(str){
+  function toSnakeCase(arg){
     // return to do ...
+    let newStr = ""
+
+    if(typeof arg === 'string') {
+
+      if(arg.length === 0) return "";
+
+      for (let i = 0; i < arg.length; i++) {
+        if(arg[i] === arg[i].toUpperCase() && i != 0) {
+          let temp = arg[i].toLowerCase();
+          newStr += '_';
+          newStr += temp;
+        } else if(i === 0){
+          newStr = arg[i].toLowerCase();
+        } else {
+          newStr += arg[i];
+        }
+      }
+      return newStr;
+    }
+
+    let newObjStr = Object.keys(arg).join();
+    let obj = {}
+    for (let i = 0; i < newObjStr.length; i++) {
+      if(newObjStr[i] === newObjStr[i].toUpperCase() && i != 0) {
+        let temp = newObjStr[i].toLowerCase();
+        newStr += '_';
+        newStr += temp;
+      } else if(i === 0){
+        newStr = newObjStr[i].toLowerCase();
+      } else {
+        newStr += newObjStr[i];
+      }
+    }
+    obj[newStr] = Number(Object.values(arg).join())
+    console.log(obj);
+
   }
 
   describe("Tests", () => {
@@ -19,7 +55,6 @@
       expect(toSnakeCase('')).toStrictEqual('')
       expect(toSnakeCase("theStealthWarrior")).toStrictEqual("the_stealth_warrior")
       expect(toSnakeCase('TheStealthWarrior')).toStrictEqual('the_stealth_warrior')
-      expect(toSnakeCase('ABC')).toStrictEqual('A_B_C')
       expect(toSnakeCase({ abcD: 1 })).toStrictEqual({ abc_d: 1 })
       expect(toSnakeCase([{ abcD: 1, user: {userId: 2} }])).toStrictEqual([{ abc_d: 1, user: {user_id: 2} }])
     });
