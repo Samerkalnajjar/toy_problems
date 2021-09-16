2a0974bb04ec034c8d1a3df59e632f5df3f3fc6b
Samerkalnajjar <samer.alnajjar@optimumpartners.co>
2021-09-16 11:40:35 +0300

    balancedParens

--- balancedParns/index.test.js
+++ balancedParns/index.test.js
@@ -24,6 +24,51 @@
  *	"())"
  */
 
+//  Unit testing:
+
+test("“Happy Path” - Expected outcome", () => {
+  expect(balancedParens("{}(){}")).toBeTruthy();
+})
+
+test("Expected failure", () => {
+  expect(balancedParens("{}({}")).toBeFalsy();
+})
+
+test("Edge case", () => {
+  expect(balancedParens("")).toEqual("Please enter a valid string");
+})
+
  function balancedParens(input) {
   //  TO DO
+  if(input === "") return "Please enter a valid string";
+
+  let Parentheses = 0;
+  let Bracket = 0;
+  let CurlyBracket = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if("(" === input[i]) {
+      Parentheses++;
+    } else if(")" === input[i]) {
+      Parentheses++;
+    } else if("[" === input[i]) {
+      Bracket++;
+    } else if("]" === input[i]) {
+      Bracket++;
+    } else if("{" === input[i]) {
+      CurlyBracket++;
+    } else if("}" === input[i]) {
+      CurlyBracket++;
+    }
+  }
+
+
+  if(Parentheses > 0 ||  Bracket > 0 || CurlyBracket > 0) {
+    if(Parentheses % 2 === 0 && Bracket % 2 === 0 && CurlyBracket % 2 === 0) {
+      return true;
+    }
+  } else {
+    return false
+  }
+    return false
 }
