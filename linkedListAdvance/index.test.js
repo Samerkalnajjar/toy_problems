5f659f8554da115bd9af5abc71a1238409940cc2
Samerkalnajjar <samer.alnajjar@optimumpartners.co>
2021-09-26 11:38:31 +0300

    pseudoclassical instantiation pattern

--- linkedListAdvance/index.test.js
+++ linkedListAdvance/index.test.js
@@ -18,9 +18,73 @@
 // list.removeHead(); //yields '4'
 // list.tail.value;   //yields '5';
 
+const Node = function(data = null) {
+  this.data = data
+  this.next = null
+}
 
-const LinkedList = () => {
+
+const LinkedList = function() {
   //fill me in!
+  this.head = null;
+  this.length = 0;
+  this.tail = null;
 };
 
 //write methods here!
+
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value);
+  this.length++;
+  if(!this.head){
+      this.head = newNode;
+  } else {
+      let lastNode = this.head;
+      while(lastNode.next) {
+          lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+  }
+}
+
+LinkedList.prototype.removeHead = function() {
+  this.head = this.head.next;
+}
+
+
+LinkedList.prototype.contains = function(val) {
+  let current = this.head;
+
+  while(current) {
+    if(current.value === val) {
+      return true;
+    }
+    current = current.next;
+  }
+  return false;
+}
+
+describe("LinkedList", () => {
+  it("checkTail", () => {
+    const list = new LinkedList();
+    expect(list.tail).toEqual(null)
+  })
+
+  it("addToTail", () => {
+    const list = new LinkedList();
+    expect(list.addToTail(4)).toEqual(list.head.value)
+  })
+
+  it("contains", () => {
+    const list = new LinkedList();
+    list.addToTail(4);
+    list.addToTail(5);
+    expect(list.contains(5)).toEqual(true)
+  })
+
+  it("removeHead", () => {
+    const list = new LinkedList();
+    list.addToTail(4);
+    expect(list.removeHead()).toEqual(4)
+  })
+})
