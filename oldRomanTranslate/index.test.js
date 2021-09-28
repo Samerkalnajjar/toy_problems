ad1cc7741eae62474a6b07ac58d2f7b4f2eafdd3
Samerkalnajjar <samer.alnajjar@optimumpartners.co>
2021-09-28 11:20:12 +0300

    numerical number

--- oldRomanTranslate/index.test.js
+++ oldRomanTranslate/index.test.js
@@ -28,4 +28,37 @@ const DIGIT_VALUES = {
 
  const translateRomanNumeral = (romanNumeral)=> {
    // TO DO ...
+   let num = 0;
+   for (let index = 0; index < romanNumeral.length; index++) {
+    if(romanNumeral[index] === "I") {
+      if(romanNumeral[index+1] !== "I" && romanNumeral[index+1] !== undefined) {
+        num-=1
+        continue;
+      }
+      num += 1
+    } else if(romanNumeral[index] === "I") {
+      num += 5
+    } else if(romanNumeral[index] === "V") {
+      num += 5
+    } else if(romanNumeral[index] === "X") {
+      num += 10
+    } else if(romanNumeral[index] === "L") {
+      num += 50
+    } else if(romanNumeral[index] === "C") {
+      num += 100
+    } else if(romanNumeral[index] === "D") {
+      num += 500
+    } else if(romanNumeral[index] === "M") {
+      num += 1000
+    }
+   }
+
+   return num;
   }
+
+  describe("translateRomanNumeral", () => {
+    it("XXI", () => {
+      const romanNumeral = "XXI";
+      expect(translateRomanNumeral(romanNumeral)).toEqual(21)
+    })
+  })
