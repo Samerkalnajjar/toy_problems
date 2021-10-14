9dededb2d62c7a7c82fe693f45553bb824a25d33
Samerkalnajjar <samer.alnajjar@optimumpartners.co>
2021-10-14 11:14:28 +0300

    secondMax

--- secondMax/index.test.js
+++ secondMax/index.test.js
@@ -12,4 +12,28 @@ secondMax([-1, -5, 2])== -1
 
 const secondMax = function(array) {
   // TODO
+  let max = array[0];
+  let secondMax = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (secondMax < array[i] && array[i] !== max) {
+      secondMax = array[i];
+    }
+  }
+  return secondMax;
 };
+
+
+describe('secondMax functions', () => {
+  it('steps are positive', () => {
+    expect(secondMax([1, 5, 2])).toEqual(2);
+  });
+  it('steps are negative', () => {
+    expect(secondMax([-1, -5, 2])).toEqual(-1);
+  });
+});
